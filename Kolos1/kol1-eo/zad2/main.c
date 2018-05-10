66241a440be4c49ebe86880ce4511fb7f9b0016d
MarcinAman <marcin.aman@gmail.com>
2018-05-10 12:48:35 +0200

    .

--- Kolos1/kol1-eo/zad2/main.c
+++ Kolos1/kol1-eo/zad2/main.c
@@ -5,10 +5,17 @@
 #include <stdio.h>
 #include <signal.h>
 
+int didChildEnd = 0;
+
+void randomHandler(int signo){
+  printf("Child ended\n");
+  didChildEnd = 1;
+}
+
 int main()
 {
   pid_t child;
-  int status, retval;
+  signal(SIGCHLD,randomHandler);
   if((child = fork()) < 0) {
     perror("fork");
     exit(EXIT_FAILURE);
@@ -23,6 +30,23 @@ int main()
  * Jeśli wysłanie sygnału się nie powiodło, ponownie oczekuje na zakończenie procesu child,
  * tym razem zawieszając pracę do czasu zakończenia sygnału
  * jeśli się powiodło, wypisuje komunikat sukcesu zakończenia procesu potomka z numerem jego PID i statusem zakończenia. */
+
+  sleep(1);
+
+  int exit_code;
+
+  if(didChildEnd==0){
+    kill(child,SIGKILL);
+  }
+
+  sleep(1);
+
+  if(waitpid(child,&exit_code,WNOHANG)==-1){
+    printf("error on wait pid\n");
+    exit(1);
+  }
+
+  printf("Exit code:%d\n",WEXITSTATUS(exit_code));
 
 
 /* koniec*/ 
--- Kolos1/kol1-eo/zad3/main.c
+++ Kolos1/kol1-eo/zad3/main.c
@@ -11,24 +11,24 @@ int main(int argc, char* argv[]) {
 
 	switch(fork()){
 	  case -1:
-		perror("FORK");
-		return 2;
+			perror("FORK");
+			return 2;
 	  case 0:
-		dup2(pdesk[1], STDOUT_FILENO);
-		execvp("ls", argv);
-		perror("EXECVP ls");
-		exit(1);
+			dup2(pdesk[1], STDOUT_FILENO);
+			execvp("ls", argv);
+			perror("EXECVP ls");
+			exit(1);
 	  default: {
    /* Zamknij deskryptor do zapisu,
     * skopiuj deskyptor końca do odczytu tego potoku na deskryptor wejścia standardowego,
     * wykonaj  tr "a-z" "A-Z", w przypadku błędu  obsłuż go i wyjdź, zwracając 3.
    */
-
-
-
-
-
-
+		close(pdesk[1]);
+		if(dup2(pdesk[0],STDIN_FILENO)==-1){
+			close(pdesk[0]);
+			return 3;
+		}
+		execlp("tr","tr","a-z","A-Z",(char*)NULL);
 
    /* koniec */
        	 }
